42aade2eec3f426dd3bb53777df534c4cd721870
qyy752457002 <yiyuqianunitediting@gmail.com>
2026-02-28 01:31:11 +0000

    update training and test files

--- examples/qwen3_vl_2b_CLEVR_grpo.sh
+++ examples/qwen3_vl_2b_CLEVR_grpo.sh
@@ -7,8 +7,8 @@ MODEL_PATH=Qwen/Qwen3-VL-2B-Instruct
  
 python3 -m verl.trainer.main \
     config=./examples/clevr_config.yaml \
-    data.train_files=./examples/CLEVR/data/train-00000-of-00008.parquet  \
-    data.val_files=./examples/CLEVR/data/test-00000-of-00001.parquet \
+    data.train_files=./examples/CLEVR/data/train-00000-of-00010.parquet  \
+    data.val_files=./examples/CLEVR/data/test-00000-of-00002.parquet \
     worker.actor.model.model_path=${MODEL_PATH} \
     trainer.experiment_name=qwen3_vl_2b_CLEVR_grpo \
     trainer.n_gpus_per_node=4 \
